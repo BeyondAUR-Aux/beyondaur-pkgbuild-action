7add85ddfbaded88e5d3832d039a8b2fecaa3665
evan0greenup <44539531+evan0greenup@users.noreply.github.com>
2024-06-29 03:03:02 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,22 @@ set -euo pipefail
 
 FILE="$(basename "$0")"
 
+# Makepkg does not allow running as root
+# Create a new user `builder`
+# `builder` needs to have a home directory because some PKGBUILDs will try to
+# write to it (e.g. for cache)
+useradd builder -m
+# When installing dependencies, makepkg will use sudo
+# Give user `builder` passwordless sudo access
+echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
+sudo -H -u builder ghcup install stack latest
+sudo -H -u builder ghcup install ghc latest
+sudo -H -u builder ghcup install cabal latest
+
+sudo -H -u builder ghcup set stack latest
+sudo -H -u builder ghcup set ghc latest
+sudo -H -u builder ghcup set cabal latest
 
 # Give all users (particularly builder) full access to these files
 chmod -R a+rw .
