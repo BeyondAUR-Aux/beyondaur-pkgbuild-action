c9562e5ba4ca520a416c238e054b8bdfe0826937
evan0greenup <44539531+evan0greenup@users.noreply.github.com>
2024-06-29 09:19:32 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,35 +20,6 @@ sudo -H -u builder ghcup set stack recommended
 sudo -H -u builder ghcup set ghc recommended
 sudo -H -u builder ghcup set cabal recommended
 
-cat << EOF > /home/builder/.stack/hooks/ghc-install.sh
-#!/bin/sh
-
-# !! KEEP THIS SCRIPT POSIX COMPLIANT !!
-
-# see https://docs.haskellstack.org/en/stable/yaml_configuration/#ghc-installation-customisation-experimental
-# for documentation about hooks
-
-set -eu
-
-case $HOOK_GHC_TYPE in
-    bindist)
-        ghc_path=$(ghcup whereis ghc "$HOOK_GHC_VERSION" || { ghcup install ghc "$HOOK_GHC_VERSION" >/dev/null && ghcup whereis ghc "$HOOK_GHC_VERSION" ; }) || { >&2 echo "Installing $HOOK_GHC_VERSION via ghcup failed" exit 3 ;}
-        printf "%s" "${ghc_path}"
-        ;;
-    git)
-        # TODO: should be somewhat possible
-        >&2 echo "Hook doesn't support installing from source"
-        exit 1
-        ;;
-    *)
-        >&2 echo "Unsupported GHC installation type: $HOOK_GHC_TYPE"
-        exit 2
-        ;;
-esac
-EOF
-chmod +x /home/builder/.stack/hooks/ghc-install.sh
-chown builder /home/builder/.stack/hooks/ghc-install.sh
-
 sudo -H -u builder stack config set system-ghc false --global
 sudo -H -u builder stack config set install-ghc false --global
 
